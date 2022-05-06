da16214acbed9de561fed55080c58298d010bbcd
getwlad <wladmir.rrodrigues@gmail.com>
2022-05-06 15:14:35 -0300

    lista-de-tarefas

---
+++ src/js/index.js
@@ -0,0 +1,65 @@
+//Criação de elementos
+const input = document.querySelector(".input-tarefa");
+// funções
+const createElement = (tagName, className) => {
+  const element = document.createElement(tagName);
+  if (className && className !== undefined) {
+    Array.isArray(className)
+      ? className.forEach((clss) => element.classList.add(clss))
+      : element.classList.add(className);
+  }
+  return element;
+};
+const erase = () => {
+  input.value = "";
+};
+const submit = () => {
+  if (!input.value || input.value === undefined || input.value === "") {
+    return;
+  }
+  //li
+  const taskItem = createElement("li");
+  //botao
+  const trashBtn = createElement("button", "btn-remove");
+  trashBtn.onclick = removerItem;
+  const iconBtn = createElement("i", ["fa-solid", "fa-trash"]);
+  trashBtn.appendChild(iconBtn);
+  //texto
+  const spanItem = createElement("span");
+  spanItem.innerText = input.value;
+  spanItem.onclick = tarefaConcluida;
+  // inserir itens no li
+  taskItem.appendChild(spanItem);
+  taskItem.appendChild(trashBtn);
+  //inserir li na lista de tarefas
+  document.querySelector("#tarefas-pendentes").appendChild(taskItem);
+
+  erase();
+};
+
+const teclaPressionada = (e) => {
+  if (e.keyCode === 13) {
+    submit();
+  }
+  if (e.keyCode === 27) {
+    erase();
+  }
+};
+
+const removerItem = (e) => {
+  e.target.tagName === "I"
+    ? e.target.parentNode.parentNode.remove()
+    : e.target.parentNode.remove();
+};
+const tarefaConcluida = (e) => {
+  const id = e.target.parentNode.parentNode.id;
+  if (id === "tarefas-pendentes") {
+    const tarefasConc = document.querySelector("#tarefa-concluida");
+    tarefasConc.appendChild(e.target.parentNode);
+  } else {
+    const tarefasPend = document.querySelector("#tarefas-pendentes");
+    tarefasPend.appendChild(e.target.parentNode);
+  }
+};
+document.querySelector(".btn-tarefa").addEventListener("click", submit);
+input.addEventListener("keydown", teclaPressionada);
